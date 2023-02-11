8c0e7e51032ad58bff32d3a1533e527f59419885
mrpaulishaili <paulishaili@gmail.com>
2023-02-11 07:28:51 +0100

    ...

--- components/footer.jsx
+++ components/footer.jsx
@@ -10,33 +10,32 @@ export default function footer() {
   };
 
   return (
-    <footer className="bg-gray-50 p-5" style={bg}>
+    <footer className="bg-green-500 p-3 px-[2%]">
       <Newslatter></Newslatter>
-      <div className="container mx-auto flex justify-center py-12">
-        <div className="py-5">
+      <div className="container mx-auto flex justify-center py-8">
+        <div className="py-3">
           <div className="flex gap-6 justify-center">
             <Link href={'/'}>
               <a>
-                <ImFacebook color="#888888" />
+                <ImFacebook color="white" />
               </a>
             </Link>
             <Link href={'/'}>
               <a>
-                <ImTwitter color="#888888" />
+                <ImTwitter color="white" />
               </a>
             </Link>
             <Link href={'/'}>
               <a>
-                <ImYoutube color="#888888" />
+                <ImYoutube color="white" />
               </a>
             </Link>
           </div>
 
-          <p className="py-5 text-gray-400">
-            Copyright ©2022 All rights reserved | This template is made with by
-            Daily Tuition
+          <p className="py-5 text-gray-100 text-center">
+            Copyright ©2023 All rights reserved | A product of APPHEART
           </p>
-          <p className="text-gray-400 text-center">Terms & Condition</p>
+          <p className="text-gray-100 text-center">Terms & Condition</p>
         </div>
       </div>
     </footer>
